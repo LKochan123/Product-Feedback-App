9d0408b2f7cc9f6a712b5dda79598091b1bcca1c
Michał Brzeziński <michalbrzezinski@Michas-MacBook-Pro.local>
2023-07-28 02:21:33 +0200

    cr

--- Product-Feedback-App/src/app/auth/features/signup/signup.component.ts
+++ Product-Feedback-App/src/app/auth/features/signup/signup.component.ts
@@ -36,7 +36,7 @@ export class SignupComponent implements OnInit {
 
   private createSignUpForm() {
     //Formularz nie jest otypowany
-    //zadne z tych pull nigdy nie jest nullem!
+    //zadne z tych pol nigdy nie jest nullem!
     this.signUpForm = new FormGroup({
       username: new FormControl(null, {
         validators: [Validators.required, Validators.maxLength(20), Validators.pattern(/^\S*$/)],
